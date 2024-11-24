de5029124a6cf3bd2ae8152803444b59c4c151c5
ishi-ta-lal <ishita.lal01@gmail.com>
2024-11-24 23:22:34 +0530

    socket implemented

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,28 +1,24 @@
 import useGetConversations from "../../hooks/useGetConversations";
-import { getRandomEmoji } from "../../utils/emoji";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-  const { loading, conversations } = useGetConversations();
-  console.log("CONVERSATIONS: ", conversations);
-  return (
-    <div className="py-2 flex flex-col overflow-auto">
-      {conversations.map((conversation, idx) => (
-        <Conversation
-          key={conversation._id}
-          conversation={conversation}
-          emoji={getRandomEmoji()}
-          lastIdx={idx === conversations.length - 1}
-        />
-      ))}
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
-      {loading ? (
-        <span className="loading loading-spinner mx-auto"></span>
-      ) : null}
-    </div>
-  );
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
 };
-
 export default Conversations;
 
 //STARTER CODE FOR THIS FILE
